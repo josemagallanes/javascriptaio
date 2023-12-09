7002a22c308c2c5611e6026dff10be77049f5320
Jose Magallanes <jose-magallanes@live.com>
2023-12-09 09:18:19 -0600

    more jS examples

---
+++ 1.8/movies.js
@@ -0,0 +1,18 @@
+scentOfAWoman = {
+  title: "Scent of a Woman",
+  id: "0001",
+};
+theGodfather = {
+  title: "The Godfather",
+  id: "0002",
+};
+cruisin = {
+  title: "Cruisin'",
+  id: "0003",
+};
+dogDayAfternoon = {
+  title: "Dog Day Afternoon",
+  id: "0004",
+};
+
+movies = [scentOfAWoman, theGodfather, cruisin, dogDayAfternoon];
